9fa096e8afdec1a3f36a2a874b7d60c529ddd40a
Philip Nerrison Boadu <philipnerison26@gmail.com>
2023-07-13 17:55:21 +0000

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,13 @@
 #include "alx.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * @str_concat: concatenates two strings
+ * @s: The returned pointer should point to a newly allocated space in memory which contains the contents of s1,followed by the contents of s2, and null terminated.
+ * if NULL is passed, treat it as an empty string
+ * The function should return NULL on failure
+ */
+
 
 char *str_concat(char *s1, char *s2)
 {
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * Each element of the grid should be initialized to 0
+ * The function should return NULL on failure
+ * If width or height is 0 or negative, return NULL
+ */
 int **alloc_grid(int width, int height)
 {
   int i, j;
